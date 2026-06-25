cb62459697f2c0b2b85bb7c00deb1a4ba850f930
possior <153281819+possior@users.noreply.github.com>
2026-06-25 15:57:15 +0900

    20260625 | Modified /source/wezterm.lua

--- source/wezterm.lua
+++ source/wezterm.lua
@@ -1,4 +1,9 @@
 local wezterm = require("wezterm")
 local configuration = wezterm.config_builder()
+local ansi = {"rgb(000% 000% 000%)", "rgb(000% 010% 000%)", "rgb(000% 020% 000%)", "rgb(000% 030% 000%)", "rgb(000% 040% 000%)", "rgb(000% 050% 000%)", "rgb(000% 060% 000%)", "rgb(000% 070% 000%)"}
+local brights = {"rgb(000% 030% 000%)", "rgb(000% 040% 000%)", "rgb(000% 050% 000%)", "rgb(000% 060% 000%)", "rgb(000% 070% 000%)", "rgb(000% 080% 000%)", "rgb(000% 090% 000%)", "rgb(000% 100% 000%)"}
 configuration.automatically_reload_config = true
+configuration.colors = {}
+configuration.colors.ansi = ansi
+configuration.colors.brights = brights
 return configuration
