40d90da2e7224469cf7892d222a70bb982ebb6f4
possior <153281819+possior@users.noreply.github.com>
2026-07-13 17:53:36 +0900

    20260713 | Modified /src/wezterm.lua

--- src/wezterm.lua
+++ src/wezterm.lua
@@ -27,10 +27,12 @@ configuration.window_background_opacity = 0.8
 
 configuration.disable_default_key_bindings = true
 configuration.keys = {
+    {key = "Tab", mods = "CTRL", action = action.ActivateTabRelative(1)},
     {key = "=", mods = "CTRL", action = action.IncreaseFontSize},
     {key = "-", mods = "CTRL", action = action.DecreaseFontSize},
     {key = "n", mods = "CTRL", action = action.SpawnTab("CurrentPaneDomain")},
     {key = "w", mods = "CTRL", action = action.CloseCurrentTab({confirm = true})},
+    {key = "Tab", mods = "SHIFT|CTRL", action = action.ActivateTabRelative(-1)},
     {key = "+", mods = "SHIFT|CTRL", action = action.IncreaseFontSize},
     {key = "_", mods = "SHIFT|CTRL", action = action.DecreaseFontSize},
     {key = "N", mods = "SHIFT|CTRL", action = action.SpawnWindow},
