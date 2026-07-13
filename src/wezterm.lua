c1712c03320e10a6327170a81238902e2dff6dd0
possior <153281819+possior@users.noreply.github.com>
2026-07-13 12:09:07 +0900

    20260713 | Modified /src/wezterm.lua

--- src/wezterm.lua
+++ src/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local action = wezterm.action
+
 local configuration = wezterm.config_builder()
 local ansi_dark = {"#000000", "#006300", "#006F00", "#007B00", "#008700", "#009300", "#009F00", "#00AB00"}
 local brights_dark = {"#000000", "#00B700", "#00C300", "#00CF00", "#00DB00", "#00E700", "#00F300", "#00FF00"}
@@ -7,6 +8,7 @@ local ansi_light = {"#FFFFFF", "#009C00", "#009000", "#008400", "#007800", "#006
 local brights_light = {"#FFFFFF", "#004800", "#003C00", "#003000", "#002400", "#001800", "#000C00", "#000000"}
 local ansi = ansi_dark
 local brights = brights_dark
+
 configuration.automatically_reload_config = true
 configuration.colors = {}
 configuration.colors.ansi = ansi
@@ -22,5 +24,7 @@ configuration.font = wezterm.font_with_fallback({"Liberation Mono", "IPAexGothic
 configuration.kde_window_background_blur = true
 configuration.use_ime = true
 configuration.window_background_opacity = 0.8
+
 configuration.disable_default_key_bindings = false
+
 return configuration
