4baa6931dcca3b0f6408bc690e3da9c94edaf487
possior <153281819+possior@users.noreply.github.com>
2026-07-13 19:03:40 +0900

    20260713 | Modified /src/wezterm.lua

--- src/wezterm.lua
+++ src/wezterm.lua
@@ -30,13 +30,17 @@ configuration.keys = {
     {key = "Tab", mods = "CTRL", action = action.ActivateTabRelative(1)},
     {key = "=", mods = "CTRL", action = action.IncreaseFontSize},
     {key = "-", mods = "CTRL", action = action.DecreaseFontSize},
+    {key = "N", mods = "CTRL", action = action.SpawnTab("CurrentPaneDomain")},
     {key = "n", mods = "CTRL", action = action.SpawnTab("CurrentPaneDomain")},
+    {key = "W", mods = "CTRL", action = action.CloseCurrentTab({confirm = true})},
     {key = "w", mods = "CTRL", action = action.CloseCurrentTab({confirm = true})},
     {key = "Tab", mods = "SHIFT|CTRL", action = action.ActivateTabRelative(-1)},
     {key = "+", mods = "SHIFT|CTRL", action = action.IncreaseFontSize},
     {key = "_", mods = "SHIFT|CTRL", action = action.DecreaseFontSize},
     {key = "N", mods = "SHIFT|CTRL", action = action.SpawnWindow},
-    {key = "W", mods = "SHIFT|CTRL", action = action.CloseCurrentTab({confirm = true})}
+    {key = "n", mods = "SHIFT|CTRL", action = action.SpawnWindow},
+    {key = "W", mods = "SHIFT|CTRL", action = action.CloseCurrentTab({confirm = true})},
+    {key = "w", mods = "SHIFT|CTRL", action = action.CloseCurrentTab({confirm = true})}
 }
 
 return configuration
