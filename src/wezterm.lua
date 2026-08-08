e892a42090651afceac89695e1d06b416382371b
possior <153281819+possior@users.noreply.github.com>
2026-08-08 16:57:07 +0900

    20260808 | Modified /src/wezterm.lua | Deprecated warning to the blur setting #2

--- src/wezterm.lua
+++ src/wezterm.lua
@@ -21,7 +21,7 @@ configuration.colors.foreground = ansi[8]
 configuration.colors.selection_bg = brights[2]
 configuration.colors.selection_fg = brights[1]
 configuration.font = wezterm.font_with_fallback({"Liberation Mono", "IPAexGothic", "Symbols Nerd Font Mono"})
-configuration.kde_window_background_blur = true
+configuration.wayland_window_background_blur = true
 configuration.use_ime = true
 configuration.window_background_opacity = 0.8
 
